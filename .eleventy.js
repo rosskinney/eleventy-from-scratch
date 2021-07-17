4af982380985303c7b17e6cb01fc1bc9ea0bb5d3
Ross Kinney <ross@rossgkinney.com>
2021-07-16 20:58:37 -0600

    Initial setup for Nunjucks processing.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
 module.exports = config => {
+
   return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+
     dir: {
       input: 'src',
       output: 'dist'
