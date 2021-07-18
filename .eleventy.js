3af950e7a6c048c4496e43ce37eb66eca29c130d
Ross Kinney <ross@rossgkinney.com>
2021-07-18 12:24:47 -0600

    Configuration for blog pages including pagaination.

--- .eleventy.js
+++ .eleventy.js
@@ -16,6 +16,12 @@ config.addCollection('featuredWork', collection => {
   );
 });
 
+// Returns a collection of blog posts in reverse date order
+config.addCollection('blog', collection => {
+  return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+});
+
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
